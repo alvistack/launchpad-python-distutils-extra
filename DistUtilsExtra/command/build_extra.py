640ec4bfd8febb8683881d230bc1ce2369a7b3e0
Sebastian Heinlein <sebi@sebi-laptop>
2007-05-01 11:13:45 +0200

    * do not add the custom commands to cmdclass in build_extra * process the custom build commands as sub commands in build_extra if   they are set in cmdclass

--- DistUtilsExtra/command/build_extra.py
+++ DistUtilsExtra/command/build_extra.py
@@ -7,22 +7,14 @@ import os.path
 import re
 import sys
 import distutils.command.build
-from build_l10n import build_l10n
-from build_help import build_help
-from build_icons import build_icons
 
 class build_extra(distutils.command.build.build):
     def __init__(self, dist):
         distutils.command.build.build.__init__(self, dist)
 
-        self.user_options.extend([("l10n", "l", "use the localsation"),
-                                  ("icons", "i", "use icons"),
-                                  ("help", "h", "use help system")])
-        #FIXME: should be part of the distribution instance
-        self.distribution.cmdclass["build_l10n"] = build_l10n
-        self.distribution.cmdclass["build_help"] = build_icons
-        self.distribution.cmdclass["build_icons"] = build_icons
-
+        self.user_options.extend([("l10n", None, "use the localsation"),
+                                  ("icons", None, "use icons"),
+                                  ("help", None, "use help system")])
     def initialize_options(self):
         distutils.command.build.build.initialize_options(self)
         self.l10n = False
@@ -31,14 +23,17 @@ class build_extra(distutils.command.build.build):
 
     def finalize_options(self):
         def has_help(command):
-            #FIXME: should be part of the distribution instance
-            return self.help == "True"
+            return self.help == "True" or \
+                   (self.distribution.cmdclass.has_key("build_help") and not \
+                    self.help == "False")
         def has_icons(command):
-            #FIXME: should be part of the distribution instance
-            return self.icons == "True"
+            return self.icons == "True" or \
+                   (self.distribution.cmdclass.has_key("build_icons") and not \
+                    self.help == "False")
         def has_l10n(command):
-            #FIXME: should be part of the distribution instance
-            return self.l10n == "True"
+            return self.l10n == "True" or \
+                   (self.distribution.cmdclass.has_key("build_l10n") and not \
+                    self.l10n == "False")
         distutils.command.build.build.finalize_options(self)
         self.sub_commands.append(("build_l10n", has_l10n))
         self.sub_commands.append(("build_icons", has_icons))
