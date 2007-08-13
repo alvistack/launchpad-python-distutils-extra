f9b3af9fd71fba6fca0d05483cabd68f4d3d7c8a
Sebastian Heinlein <renate@renate-laptop>
2007-08-13 14:26:31 +0200

    * setuptools support

--- DistUtilsExtra/command/build_extra.py
+++ DistUtilsExtra/command/build_extra.py
@@ -9,6 +9,8 @@ import sys
 import distutils.command.build
 
 class build_extra(distutils.command.build.build):
+    """Adds the extra commands to the build target. This class should be used
+       with the core distutils"""
     def __init__(self, dist):
         distutils.command.build.build.__init__(self, dist)
 
@@ -34,6 +36,21 @@ class build_extra(distutils.command.build.build):
             return self.i18n == "True" or \
                    (self.distribution.cmdclass.has_key("build_i18n") and not \
                     self.i18n == "False")
+        distutils.command.build.build.finalize_options(self)
+        self.sub_commands.append(("build_i18n", has_i18n))
+        self.sub_commands.append(("build_icons", has_icons))
+        self.sub_commands.append(("build_help", has_help))
+
+class build(build_extra):
+    """Adds the extra commands to the build target. This class should be
+       used with setuptools."""
+    def finalize_options(self):
+        def has_help(command):
+            return self.help == "True"
+        def has_icons(command):
+            return self.icons == "True"
+        def has_i18n(command):
+            return self.i18n == "True"
         distutils.command.build.build.finalize_options(self)
         self.sub_commands.append(("build_i18n", has_i18n))
         self.sub_commands.append(("build_icons", has_icons))
