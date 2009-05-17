5e1a5eec8ff6a948a8ca1ac0c630c3ddf033a316
Martin Pitt <mpitt@debian.org>
2009-05-17 16:00:56 +0200

    find packages

--- DistUtilsExtra/auto.py
+++ DistUtilsExtra/auto.py
@@ -8,7 +8,7 @@ for GtkBilder, D-Bus, PolicyKit files, scripts, etc.
 # (c) 2009 Canonical Ltd.
 # Author: Martin Pitt <martin.pitt@ubuntu.com>
 
-import os, os.path
+import os, os.path, fnmatch
 import distutils.core
 
 from DistUtilsExtra.command import *
@@ -18,7 +18,7 @@ def setup(**attrs):
 
     TODO: doc
     '''
-    src = find_src_files()
+    src = src_find()
 
     print '---- attrs before: ----'
     print attrs
@@ -26,6 +26,7 @@ def setup(**attrs):
     print src
 
     __cmdclass(attrs)
+    __packages(attrs, src)
 
     print '---- attrs after: ----'
     print attrs
@@ -33,8 +34,6 @@ def setup(**attrs):
     print 'WARNING: the following files are not recognized by DistUtilsExtra.auto:'
     for f in sorted(src):
         print ' ', f
-
-    print src
 
     distutils.core.setup(**attrs)
 
@@ -49,11 +48,27 @@ def __cmdclass(attrs):
     v.setdefault('build_i18n', build_i18n.build_i18n)
     v.setdefault('build_icons', build_icons.build_icons)
 
+def __packages(attrs, src):
+    '''Default packages'''
+
+    if 'packages' in attrs:
+        for pkg in attrs['packages']:
+            src_mark(src, os.path.join(pkg, '*.py'))
+        return
+
+    packages = attrs.setdefault('packages', [])
+
+    for f in src_fileglob(src, '__init__.py'):
+        pkg = os.path.dirname(f)
+        packages.append(pkg)
+        src_mark(src, os.path.join(pkg, '*.py'))
+
+
 #
 # helper functions
 #
 
-def find_src_files():
+def src_find():
     '''Find source files.'''
 
     src = set()
@@ -76,3 +91,19 @@ def find_src_files():
             src.add(os.path.join(root, f))
 
     return src
+
+def src_fileglob(src, fnameglob):
+    '''Return set of files which match fnameglob.'''
+
+    result = set()
+    for f in src:
+        if fnmatch.fnmatch(os.path.basename(f), fnameglob):
+            result.add(f)
+    return result
+
+def src_mark(src, pathglob):
+    '''Remove all paths from src which match pathglob.'''
+
+    for f in src.copy():
+        if fnmatch.fnmatch(f, pathglob):
+            src.remove(f)
